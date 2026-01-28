42a267811afb9c17af549f186c5144979db1f36f
Sindhuja Bommu <bommusindhuja2@gmail.com>
2026-01-28 17:02:35 +0530

    Add files via upload

---
+++ Stringbuilder.java
@@ -0,0 +1,11 @@
+public class Stringbuilder {
+    public static void main(String[] args){
+        StringBuffer name = new StringBuffer("Rashmi");
+        name.append("Sharma");
+        System.out.println(name);
+        StringBuilder collegeName=new StringBuilder("Sri indu");
+        System.out.println(collegeName);
+
+    }
+    
+}
