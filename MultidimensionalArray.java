d489e7cf4ffb6550aab7334e680dbb97547ebb98
Sindhuja Bommu <bommusindhuja2@gmail.com>
2026-01-28 16:55:19 +0530

    Add files via upload

---
+++ MultidimensionalArray.java
@@ -0,0 +1,15 @@
+public class MultidimensionalArray {
+    public static void main(String[] args) {
+        int table[][] = {
+            {1,2,3},
+            {4,5,6}
+        };
+        for(int i = 0;i < table.length;i++){
+            for(int j= 0;j < table[i].length;j++ ){
+                System.out.println(table[i][j]);
+            }
+        }
+    }
+
+ 
+}
