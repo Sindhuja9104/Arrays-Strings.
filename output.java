e9e76e9c5914c64724690249026cc0e4b4dbc0fa
Sindhuja Bommu <bommusindhuja2@gmail.com>
2026-01-28 17:00:45 +0530

    Add files via upload

---
+++ output.java
@@ -0,0 +1,11 @@
+public class output {
+    public static void main(String[] args) {
+        String s  = "physics";
+        for(int j = 2;j < 4;j++) {
+            System.out.println(s.substring(j));
+
+        }
+    
+}
+    
+}
